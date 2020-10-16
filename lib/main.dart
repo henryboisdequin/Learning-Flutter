2dc3260a0783a5a7ec373d4069178b51f76e2b8a
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-16 09:14:47 +0530

    state, cards, assets, textfield, scroll views

--- lib/main.dart
+++ lib/main.dart
@@ -10,33 +10,92 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String myText = "Your Input";
+  TextEditingController _textController = TextEditingController();
+
+  // Component did mount in react
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // Component unmounts in flutter
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Flutter is Awesome!"),
       ),
       body: Center(
-        child: Container(
-          color: Colors.teal,
-          height: 100,
-          width: 100,
-        ),
-      ),
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/clement-h-programming.jpg",
+                        fit: BoxFit.fitWidth,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        myText,
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Source: https://unsplash.com/@clemhlrdt",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue),
+                      ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextField(
+                          controller: _textController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Thoughts on Photo",
+                            hintText: "I think it is nice...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ))),
       drawer: Drawer(
         child: ListView(
           padding: const EdgeInsets.all(0),
           children: <Widget>[
             UserAccountsDrawerHeader(
-            accountName: Text("Boisdequin Family"), 
-            accountEmail: Text("boisdequin@boisdequin.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                "https://images.unsplash.com/photo-1560090995-01632a28895b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80"
-              ),
-            )
-            ),
+                accountName: Text("Boisdequin Family"),
+                accountEmail: Text("boisdequin@boisdequin.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1560090995-01632a28895b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80"),
+                )),
             ListTile(
               leading: Icon(Icons.person),
               title: Text("Henry Boisdequin"),
@@ -55,10 +114,13 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {}, // empty function in dart
-        child: Icon(Icons.edit),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        onPressed: () {
+          myText = _textController.text;
+          setState(() {});
+        }, // empty function in dart
+        child: Icon(Icons.send),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
