58b12edc27af14462c96e236517600c44cd31ee3
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-16 08:31:07 +0530

    listview, users, avatar, images, listtiles, icons

--- lib/main.dart
+++ lib/main.dart
@@ -17,37 +17,48 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter is Awesome!"),
       ),
-      body: Container(
-        color: Colors.teal,
-        alignment: Alignment.center,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-            Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            alignment: Alignment.center,
-            color: Colors.red,
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            alignment: Alignment.center,
-            color: Colors.yellow,
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            alignment: Alignment.center,
-            color: Colors.purple,
-          ),
+      body: Center(
+        child: Container(
+          color: Colors.teal,
+          height: 100,
+          width: 100,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+            accountName: Text("Boisdequin Family"), 
+            accountEmail: Text("boisdequin@boisdequin.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1560090995-01632a28895b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80"
+              ),
+            )
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Henry Boisdequin"),
+              subtitle: Text("Learning Flutter"),
+              trailing: Icon(Icons.event_available),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Oscar Boisdequin"),
+              subtitle: Text("Playing FIFA"),
+              trailing: Icon(Icons.event_busy),
+              onTap: () {},
+            ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, // empty function in dart
+        child: Icon(Icons.edit),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
