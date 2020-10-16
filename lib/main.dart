c25eaa5553de760aada60006f7aac6137621906e
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-10-16 06:54:12 +0530

    learning containers + text

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,37 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter is Awesome!"),
       ),
-      body: Container(
-        child: Text("Hello Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          // mostly always use antiAlias
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: [
+              Colors.pink,
+              Colors.yellow,
+            ]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+              )],
+            ),
+          child: Text(
+            "I am a box...",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+        ),
       ),
     );
   }
